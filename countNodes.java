a6014cb25cfdc6e030dfcdb1430973de2aeb91b1
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-09-16 20:00:57 +0530

    Create countNodes.java

---
+++ countNodes.java
@@ -0,0 +1,12 @@
+public static int countNodes(Node root){
+        if(root==null){
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(root.children[i]!=null){
+                count+=countNodes(root.children[i]);
+            }
+        }
+        return count+1;
+    }
